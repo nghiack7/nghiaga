822f3033e83c9775344790ef082d3aa16a1b3bd7
nghĩa nguyễn <ngocnghia128@gmail.com>
2021-10-19 11:14:18 +0700

    edit file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, responses
+from fastapi import FastAPI, UploadFile, File
 import uuid
 import os
 from fastapi.responses import Response
 
 
-app = FastAPI(app = FastAPI(title=config.PROJECT_NAME, openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc"))
+app = FastAPI(title="test update image", openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
 
 db = []
 
 @app.post("/image")
-async def creat_image(file: UploadFile = File()):
+async def creat_image(file: UploadFile = File(...)):
     file.name = f"{uuid.uuid4()}.jpg"
     data = await file.read()
     db.append(data)
