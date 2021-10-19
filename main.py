d7782f0c0410a163cb840675553d45e128f436b5
Duy Nguyen <duynguyenngoc@hotmail.com>
2021-10-19 15:59:30 +0700

    add muti image and return image

--- main.py
+++ main.py
@@ -4,24 +4,33 @@ import os
 from fastapi.datastructures import Default
 from fastapi.responses import Response
 from starlette.responses import FileResponse
+from typing import List
+import uvicorn
+from fastapi.responses import FileResponse
 
 
 app = FastAPI(title="test update image", openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
 
-db = "Store/"
+db = "./store/"
 
+# add mutiple image
 @app.post("/image")
-async def creat_image(file: UploadFile = File(...)):
-    file.name = f"{uuid.uuid4()}.jpg"
-    data = await file.read()
-    with open(f"{db}{file.name}", "wb") as f:
-        f.write(data)
-    return {"filename": file.name}
+async def creat_image(
+    files: List[UploadFile] = File(...)
+):
+    for file in files:
+        file.name = f"{uuid.uuid4()}.jpg"
+        data = await file.read()
+        with open(f"{db}{file.name}", "wb") as f:
+            f.write(data)
+        return 'pass'
 
+# return image
 @app.get("/home")
 async def show_image(file_name):
-    file_name = os.listdir(db)
-    image = []
-    for i in range(0, len(file_name)-1):
-        image.append(FileResponse(f"{db}{file_name[i]}"))
-    return image
+    return FileResponse("{store}/{file_name}".format(store=db, file_name=file_name))
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++ RUN SERVICE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0',port=80, debug=True)
