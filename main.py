6d966d1ce97fabefe72c6b9746c9086613e40269
nghĩa nguyễn <ngocnghia128@gmail.com>
2021-10-19 14:23:46 +0700

    update folder store

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI, UploadFile, File
 import uuid
 import os
+from fastapi.datastructures import Default
 from fastapi.responses import Response
 
 
 app = FastAPI(title="test update image", openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
 
-db = []
+db = "Store/"
 
 @app.post("/image")
 async def creat_image(file: UploadFile = File(...)):
     file.name = f"{uuid.uuid4()}.jpg"
     data = await file.read()
-    db.append(data)
+    with open(f"{db}{file.name}", "wb") as f:
+        f.write(data)
     return {"filename": file.name}
 
 @app.get("/home")
