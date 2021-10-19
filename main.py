5dacf03cc30c9da16eaba1300b9ae12e848ecd80
nghĩa nguyễn <ngocnghia128@gmail.com>
2021-10-19 15:15:59 +0700

    add them get ma thay sai me roi

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import os
 from fastapi.datastructures import Default
 from fastapi.responses import Response
+from starlette.responses import FileResponse
 
 
 app = FastAPI(title="test update image", openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
@@ -18,6 +19,9 @@ async def creat_image(file: UploadFile = File(...)):
     return {"filename": file.name}
 
 @app.get("/home")
-async def show_image():
-    responses = Response(content=db)
-    return responses
+async def show_image(file_name):
+    file_name = os.listdir(db)
+    image = []
+    for i in range(0, len(file_name)-1):
+        image.append(FileResponse(f"{db}{file_name[i]}"))
+    return image
